14ce9d8b6b4be45193b8fafafe05abbc16cd6ab9
mehdi <mmehdi.g@gmail.com>
2015-01-12 14:38:13 +0100

    dialog manager again

--- src/dialog_manager.py
+++ src/dialog_manager.py
@@ -4,22 +4,35 @@ import rospy
 from std_msgs.msg import String
 
 
+def listen(msg):
+    """
+    process the message if it needs any process...
+    """
+    # TODO: you can remove this line if you don't like echos in dialog manager
+    say("user said \"" + msg.data + "\"")
+
+
 def say(msg):
     """
     if data == "ok, ..."
     if data == ???
     """
-    data = msg.data
+    if isinstance(msg, String):
+        data = msg.data
+    else:
+        data = msg
 
     if data[:3] == "ok,":
         print data
     elif data:
         # TODO: decide about form of string data ...
-        print "I think", data
+        print "I think,", data
 
 if __name__ == '__main__':
     rospy.init_node("walle_dm")
-    rospy.Subscriber("/walle/dialog/listen", String, say, queue_size=1)
+    rospy.Subscriber("/walle/dialog/listen", String, listen, queue_size=1)
     rospy.Subscriber("/walle/dialog/thoughts", String, say, queue_size=1)
+
+
 
     rospy.spin()
